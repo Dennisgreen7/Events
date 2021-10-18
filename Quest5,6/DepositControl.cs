95d100668161763c64734ef4f97ed99ee31e36d0
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-10-18 04:36:04 +0300

    Add files via upload

---
+++ Quest5,6/DepositControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quest5
+{
+    public partial class DepositControl : UserControl
+    {
+        static User Active = new User();
+        static UsersManagement ActiveManagement = new UsersManagement();
+        private void WhenDeposit(User obj)
+        {
+            MessageBox.Show("UserName: " + obj.Username + " Balance: " + obj.Account.Balance);
+        }
+        public DepositControl(User activeuser, UsersManagement activemanagement)
+        {
+            InitializeComponent();
+            Active = activeuser;
+            ActiveManagement = activemanagement;
+            ActiveManagement.BalanceChangedEvent += WhenDeposit;
+        }
+
+        private void bDeposit_Click(object sender, EventArgs e)
+        {
+            ActiveManagement.Deposit(Active, Convert.ToDouble(DepositBox.Text));
+        }
+
+        private void DepositControl_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Quest5,6/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Quest5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.bLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UsernameBox
+            // 
+            this.UsernameBox.Location = new System.Drawing.Point(307, 145);
+            this.UsernameBox.Name = "UsernameBox";
+            this.UsernameBox.Size = new System.Drawing.Size(192, 22);
+            this.UsernameBox.TabIndex = 0;
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(307, 186);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(192, 22);
+            this.PasswordBox.TabIndex = 1;
+            // 
+            // bLogin
+            // 
+            this.bLogin.Location = new System.Drawing.Point(345, 228);
+            this.bLogin.Name = "bLogin";
+            this.bLogin.Size = new System.Drawing.Size(102, 28);
+            this.bLogin.TabIndex = 2;
+            this.bLogin.Text = "Login";
+            this.bLogin.UseVisualStyleBackColor = true;
+            this.bLogin.Click += new System.EventHandler(this.bLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UsernameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UsernameBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Button bLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
