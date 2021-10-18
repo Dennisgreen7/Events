b220639412c5e969ed3c5577d0649e9da0c31d46
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-10-18 04:33:37 +0300

    Add files via upload

---
+++ Quest1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Quest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.bCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(279, 149);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(248, 22);
+            this.NameBox.TabIndex = 0;
+            this.NameBox.Text = "Name";
+            this.NameBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.NameBox.Click += new System.EventHandler(this.NameBox_Click);
+            this.NameBox.TextChanged += new System.EventHandler(this.NameBox_TextChanged);
+            // 
+            // bCheck
+            // 
+            this.bCheck.Location = new System.Drawing.Point(362, 191);
+            this.bCheck.Name = "bCheck";
+            this.bCheck.Size = new System.Drawing.Size(75, 23);
+            this.bCheck.TabIndex = 1;
+            this.bCheck.Text = "Check";
+            this.bCheck.UseVisualStyleBackColor = true;
+            this.bCheck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bCheck);
+            this.Controls.Add(this.NameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button bCheck;
+    }
+}
+
---
+++ Quest1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quest1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        public void LongName()
+        {
+            MessageBox.Show("Name Too Long");
+        }
+        public void NameValidation()
+        {
+            MessageBox.Show("Invalid first name");
+        }
+
+        private void NameBox_Click(object sender, EventArgs e)
+        {
+            NameBox.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Person person = new Person();
+            person.WrongNameSyntaxEvent += NameValidation;
+            person.TooLOngNameEvent += LongName;
+            person.Name = NameBox.Text;
+        }
+
+        private void NameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
