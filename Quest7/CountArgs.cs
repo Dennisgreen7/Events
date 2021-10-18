d880101ee3432f4894a504da1abd1547b6167276
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-10-18 04:37:25 +0300

    Add files via upload

---
+++ Quest7/CountArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest7
+{
+    class CountArgs:EventArgs
+    {
+        public int Count { get; set; } = 0;
+    }
+}
