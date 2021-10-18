8d17dd921501eff4b6c65721dfe63775c4bc9343
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-10-18 04:59:05 +0300

    Add files via upload

---
+++ Quest5,6/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest5
+{
+    public class Account
+    {
+        public double Balance { get; set; }
+
+        public Account(double balance)
+        {
+            Balance += balance;
+        }
+    }
+}
