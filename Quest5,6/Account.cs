5a283ae08ef210503a4529090241cf1d32b31bb2
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-10-18 04:52:17 +0300

    Update Account.cs

--- Quest5,6/Account.cs
+++ Quest5,6/Account.cs
@@ -8,7 +8,7 @@ namespace Quest5
 {
     public class Account
     {
-        public double Balance { get; set; }
+        public double Balance { get; set; } = 0;
 
         public Account(double balance)
         {
